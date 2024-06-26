bdafb6bed2cf951b7e3df4f1fe88a6122c8b381c
Patrícia Lima Massolini <patricialima.mas@gmail.com>
2024-06-26 19:22:15 -0300

    Alteração das Fontes e alteração na função sol

--- funcoesUsadas/funcoes2.py
+++ funcoesUsadas/funcoes2.py
@@ -2,19 +2,8 @@ import pygame
 
 def sol(tela,posX,posY,tamanhoSol):
     amarelo = (255, 204, 0)
-    crescendo = 0
-    
     pygame.draw.circle(tela,amarelo, (posX,posY),tamanhoSol)
-    if crescendo:
-        tamanhoSol += 1
-        if tamanhoSol >= 100:
-            crescendo = False
-    else:
-        tamanhoSol -= 1
-        if tamanhoSol <= 80:
-            crescendo = True
     
-    return tamanhoSol, crescendo
 
 
 def gerenciarTamanhoSol(tamanhoSol, crescendo):
